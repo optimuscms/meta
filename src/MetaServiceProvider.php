84b35f8f638fd88705e5dcf6a07a27262977bea0
Andy Coates <andy@acoates.co.uk>
2019-08-13 15:52:38 +0100

    Register conversion

--- src/MetaServiceProvider.php
+++ src/MetaServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace Optix\Media;
+namespace Optix\Meta;
 
+use Intervention\Image\Image;
+use Optix\Media\Facades\Conversion;
 use Illuminate\Support\ServiceProvider;
 
 class MetaServiceProvider extends ServiceProvider
@@ -14,5 +16,9 @@ class MetaServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        Conversion::register(Meta::OG_MEDIA_CONVERSION, function (Image $image) {
+            return $image->fit(1200, 630);
+        });
     }
 }
